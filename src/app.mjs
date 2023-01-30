a7a417a75725aa276bacd62331c0e74c5b5db1cc
Michał Konopka <konopek68@gmail.com>
2023-01-30 23:54:08 +0100

    feat: add tests for interaction by indexes route #1

---
+++ src/app.mjs
@@ -0,0 +1,33 @@
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+import compress from 'koa-compress';
+import zlib from 'zlib'
+import Koa from 'koa';
+import { router } from "./router.mjs";
+
+export function createApp(nodeDb) {
+    const app = new Koa();
+    app.use(cors({
+        async origin() {
+            return '*';
+        },
+    }));
+    app.use(compress({
+        threshold: 2048,
+        deflate: false,
+        br: {
+            params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: 4
+            }
+        }
+    }));
+
+
+    app.use(bodyParser());
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+
+    app.context.nodeDb = nodeDb;
+
+    return app;
+}
---
+++ src/handlers/onState.mjs
@@ -0,0 +1,35 @@
+import { LoggerFactory } from 'warp-contracts';
+
+LoggerFactory.INST.logLevel('none');
+LoggerFactory.INST.logLevel('debug', 'listener');
+
+const logger = LoggerFactory.INST.create('listener');
+
+function isTxIdValid(txId) {
+    const validTxIdRegex = /[a-z0-9_-]{43}/i;
+    return validTxIdRegex.test(txId);
+}
+
+
+export async function onNewState(message, dbUpdates) {
+    const msgObj = JSON.parse(message);
+    if (!isTxIdValid(msgObj.contractTxId)) {
+        logger.warn('Invalid contract txId');
+        return;
+    }
+
+    const lastSortKey = await dbUpdates.lastSortKey(msgObj.contractTxId);
+
+    if (msgObj.sortKey.localeCompare(lastSortKey)) {
+        await dbUpdates.upsertState(
+            msgObj.contractTxId, msgObj.sortKey, msgObj.state,
+            msgObj.node, msgObj.signature, msgObj.manifest, msgObj.stateHash);
+        await dbUpdates.upsertBalances(msgObj.contractTxId, msgObj.sortKey, msgObj.state);
+    } else {
+        logger.warn('Received state with older or equal sort key', {
+            contract: msgObj.contractTxId,
+            received: msgObj.sortKey,
+            latest: lastSortKey
+        });
+    }
+}
