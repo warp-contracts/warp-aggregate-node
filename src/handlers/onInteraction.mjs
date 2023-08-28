0e05325983d6eba886ee7256925930301aecdcdf
ppedziwiatr <ppedziwiatr@gmail.com>
2023-08-28 12:42:05 +0200

    fix: onInteraction tags check

--- src/handlers/onInteraction.mjs
+++ src/handlers/onInteraction.mjs
@@ -1,19 +1,21 @@
 export async function onNewInteraction(payload, dbUpdates) {
   const { tags, owner, id, block } = payload.interaction;
 
-  const indexesString = tags.find((tag) => tag.name === "Indexed-By");
+  if (tags) {
+    const indexesString = tags.find((tag) => tag.name === "Indexed-By");
 
-  if (indexesString) {
-    const indexes = indexesString.value.split(";");
+    if (indexesString) {
+      const indexes = indexesString.value.split(";");
 
-    if (indexes.length > 0) {
-      await dbUpdates.upsertInteraction(
-        payload.contractTxId,
-        id,
-        owner.address,
-        block.height,
-        indexes
-      );
+      if (indexes.length > 0) {
+        await dbUpdates.upsertInteraction(
+          payload.contractTxId,
+          id,
+          owner.address,
+          block.height,
+          indexes
+        );
+      }
     }
   }
 }
